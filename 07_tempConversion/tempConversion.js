300c74e1256aa3b7174a4140a372532a3f2d7de4
ceydabiber <ceydabiber3@gmail.com>
2024-09-13 22:49:05 +0300

    tempConversion first test passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // x Fahrenheit = (x -32) * 5/9 Celsius
+
+  let tempCelsius = (temp - 32) * 5 / 9;
+  tempCelsius.toFixed(1);
+  return tempCelsius
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempFahrenheit = (temp * 9/5) + 32;
+  tempFahrenheit.toFixed(1);
+  return tempFahrenheit;
 };
 
 // Do not edit below this line
