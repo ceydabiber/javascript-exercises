61c968c9829b06dd8bf20d319b67609a12020772
ceydabiber <ceydabiber3@gmail.com>
2024-09-13 21:55:25 +0300

    reverseString all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const reverseString = string.split('').reverse().join('');
+    return reverseString;
 };
 
 // Do not edit below this line
