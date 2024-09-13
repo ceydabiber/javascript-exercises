453e82d203ed6cc892c5947158bb7ac084ce589b
ceydabiber <ceydabiber3@gmail.com>
2024-09-13 21:37:59 +0300

    repeatString 4th test is passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,15 @@
 const repeatString = function(string, num) {
     let newString = string;
 
-    for( let i = 1; i < num; i++){    
-        newString += string;
+    if (num === 0){
+        return  "";
     }
-    return newString;
+    else{
+        for( let i = 1; i < num; i++){    
+            newString += string;
+        }
+        return newString;
+    }
 };
 
 // Do not edit below this line
