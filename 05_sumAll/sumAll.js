dcc23b560ace74696f0704426f67bcc620e50461
ceydabiber <ceydabiber3@gmail.com>
2024-09-13 22:31:54 +0300

    sumAll all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,21 @@
 const sumAll = function(num1, num2) {
     let sumAll = 0;
-    if (num1 > num2){
-        for(let i = num2; i<=num1; i++){
-            sumAll += i; 
-        }
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
     }
-    if (num1 < num2){
-        for(let i = num1; i<=num2; i++){
-            sumAll += i; 
+    if(Number.isInteger(num1) == false || Number.isInteger(num2) == false){
+        return "ERROR";
+    }
+    else{
+        if (num1 > num2){
+            for(let i = num2; i<=num1; i++){
+                sumAll += i; 
+            }
+        }
+        if (num1 < num2){
+            for(let i = num1; i<=num2; i++){
+                sumAll += i; 
+            }
         }
     }
 
