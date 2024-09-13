d5206f7ac8aa4f52aa3d8c4bd5a1b0ace8bb8af7
ceydabiber <ceydabiber3@gmail.com>
2024-09-13 22:26:26 +0300

    sumAll first test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sumAll = 0;
+    if (num1 > num2){
+        for(let i = num2; i<=num1; i++){
+            sumAll += i; 
+        }
+    }
+    if (num1 < num2){
+        for(let i = num1; i<=num2; i++){
+            sumAll += i; 
+        }
+    }
 
+    return sumAll;
 };
 
 // Do not edit below this line
